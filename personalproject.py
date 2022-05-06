16627ea1b053b9ac6deec7ffced93ab24f366bc9
nachojo <kjwjj486@gmail.com>
2022-05-06 18:32:45 +0900

    first-commit

--- personalproject.py
+++ personalproject.py
@@ -32,16 +32,25 @@ def alarm():
 
 
 window = Tk()
-window.geometry('410x210+500+300')
+window.geometry('410x180+500+300')
 set_time = StringVar()
 window.title("program")
-title_label = Label(window, text="제목입니다").grid(row=0, column=0)
+title_label = Label(window, text="제목입니다").grid(row=0, columnspan=2)
 input_exam = Label(window, text="시간을 00:00:00으로 입력해주세요.").grid(
     row=1, columnspan=2)
 
-mac_entry = Entry(window, textvariable=set_time).grid(row=2, column=0)
+mac_entry = Entry(window, width=15, textvariable=set_time).grid(
+    row=2, column=0)
 pr_time = Label(window)
-act_btn = Button(window, text=" 설정 ", width=20,
+act_btn = Button(window, text=" 설정 ", width=0,
                  command=alarm).grid(row=2, column=1)
 pr_time.grid(row=3, column=0)
+
+
+def fin():
+    window.destroy()
+
+
+des_btn = Button(window, text=" 종료 ", width=10,
+                 command=fin).grid(row=4, columnspan=2)
 window.mainloop()
